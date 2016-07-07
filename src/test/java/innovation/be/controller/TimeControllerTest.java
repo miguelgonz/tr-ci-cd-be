88b4d9c54eb5e3f891d1e62b8138635eb9e37ee8
Paul Bye <paul.bye@blackcatsolutions.co.uk>
2016-07-07 15:04:56 +0100

    Added test

---
+++ src/test/java/innovation/be/controller/TimeControllerTest.java
@@ -0,0 +1,15 @@
+package innovation.be.controller;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class TimeControllerTest {
+
+    @Test
+    public void shouldNotScrewUp() {
+        assertThat(true, is(true));
+    }
+
+}
